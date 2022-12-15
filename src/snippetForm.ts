d0903fbc27c3d47911b477a97cc5ae8ef8a237c6
Mubashar Javed <mubasharbravo302@gmail.com>
2022-12-16 04:34:43 +0500

    feat: :construction: showing a webview for snippet form

--- src/snippetForm.ts
+++ src/snippetForm.ts
@@ -4,9 +4,37 @@ export default function initSnippetForm(
     language: string | undefined,
     selectedText: string
 ) {
-    vscode.workspace
-        .openTextDocument({ language: language, content: selectedText })
-        .then((doc) => {
-            vscode.window.showTextDocument(doc);
-        });
+    const panel = vscode.window.createWebviewPanel(
+        "snippetForm",
+        "Snippet Form",
+        vscode.ViewColumn.One,
+        {
+            enableScripts: true,
+        }
+    );
+
+    panel.webview.html = getWebviewContent(selectedText);
+
+    panel.onDidDispose(() => {
+        // Clean up our resources
+    });
+}
+
+function getWebviewContent(selectedText: string) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snippet Form</title>
+    </head>
+    <body>
+        <h1>Snippet Form</h1>
+        <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+        <textarea id="snippet" name="snippet" rows="10" cols="30">${selectedText}</textarea>
+
+    </body>
+    </html>
+    `;
 }
