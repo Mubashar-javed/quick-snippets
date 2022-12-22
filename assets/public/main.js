16c417b90540df90afa20a5fb09ad6f631f9aa00
Mubashar Javed <mubasharbravo302@gmail.com>
2022-12-23 01:41:38 +0500

    added validations for snippet form

--- assets/public/main.js
+++ assets/public/main.js
@@ -1,13 +1,42 @@
 const vscode = acquireVsCodeApi();
+const prefixInput = document.getElementById('prefix');
+const errorDiv = document.getElementById('prefix-error');
+const saveBtn = document.getElementById('save-btn');
+const formInvalid = prefixInput.value.trim().length === 0;
 
 function handleSave() {
   const prefix = document.getElementById('prefix').value;
   const description = document.getElementById('description').value;
   const body = document.getElementById('snippet').value;
 
-  // TODO: add validations
-  const snippet = {prefix, description, body};
+  if (formInvalid) {
+    errorDiv.classList.remove('d-none');
+    return;
+  }
 
+  const snippet = {prefix, description, body};
   // send message back to extension
   vscode.postMessage({command: 'save', data: snippet});
 }
+
+prefixInput.addEventListener('blur', (event) => {
+  const currentValue = event.target.value;
+  const hasValue = currentValue.trim().length > 0;
+
+  if (hasValue) {
+    errorDiv.classList.add('d-none');
+  } else {
+    errorDiv.classList.remove('d-none');
+  }
+});
+
+prefixInput.addEventListener('keyup', (event) => {
+  const currentValue = event.target.value;
+  const hasValue = currentValue.trim().length > 0;
+
+  if (hasValue) {
+    errorDiv.classList.add('d-none');
+  } else {
+    errorDiv.classList.remove('d-none');
+  }
+});
